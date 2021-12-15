0891fc4a92a34e909cf54b91f2e67b691dbe8265
CatBSD <catbsd@qq.com>
2021-12-15 11:55:40 +0800

    v.0.2 美化列表

--- controller/tableView.go
+++ controller/tableView.go
@@ -1,15 +1,22 @@
 package controller
 
 import (
-	"fmt"
+	"os"
+	"strconv"
 	"time"
+
+	"github.com/olekukonko/tablewriter"
 )
 
 func TableView() {
 
-	var timess int64 = 1639444750958
-	tm := time.UnixMilli(timess)
+	var timess int64 = data_nCov.Results[0].UpdateTime
+	// tm := time.UnixMilli(timess)
+	// t := time.Now().Format("2006-01-02 15:04:05")
+	tm1 := time.UnixMilli(timess).Format("2006-01-02 15:04:05")
 	// fmt.Println(tm)
+	// fmt.Println(tm1)
+	// fmt.Println(t)
 
 	// y := tm.Year()
 	// m := tm.Month()
@@ -19,47 +26,82 @@ func TableView() {
 	// s := tm.Second()
 	// fmt.Println(y, m, d, h, i, s)
 	// data_nCov.Results[0].Cities[localIp_city].CurrentConfirmedCount
-	fmt.Println("时间", "城市", "现存确诊", "累计确诊", "可疑计数", "治愈数", "死亡人数")
+	// var (
+	// 	i                     = 1
+	// 	cityName              = data_nCov.Results[0].Cities[i].CityName
+	// 	currentConfirmedCount = data_nCov.Results[0].Cities[i].CurrentConfirmedCount
+	// 	confirmedCount        = data_nCov.Results[0].Cities[i].ConfirmedCount
+	// 	suspectedCount        = data_nCov.Results[0].Cities[i].SuspectedCount
+	// 	curedCount            = data_nCov.Results[0].Cities[i].CuredCount
+	// 	deadCount             = data_nCov.Results[0].Cities[i].DeadCount
+	// )
+
+	// fmt.Println("数据更新时间", "城市", "现存确诊", "累计确诊", "可疑计数", "治愈数", "死亡人数")
+	// for i := 0; i < len(data_nCov.Results[0].Cities); i++ {
+	// 	if data_nCov.Results[0].Cities[i].CurrentConfirmedCount != 0 {
+	// 		fmt.Println(tm1,
+	// 			data_nCov.Results[0].Cities[i].CityName,
+	// 			data_nCov.Results[0].Cities[i].CurrentConfirmedCount,
+	// 			data_nCov.Results[0].Cities[i].ConfirmedCount,
+	// 			data_nCov.Results[0].Cities[i].SuspectedCount,
+	// 			data_nCov.Results[0].Cities[i].CuredCount,
+	// 			data_nCov.Results[0].Cities[i].DeadCount)
+	// 	}
+	// 	// fmt.Println(tm1, cityName, currentConfirmedCount, confirmedCount, suspectedCount, curedCount, deadCount)
+	// }
+
+	// var num1 int
+	// for i := 0; i < len(data_nCov.Results[0].Cities); i++ {
+	// 	num1 = i
+	// }
+
+	// 动态创建二维数组
+	data2 := [][]string{}
 	for i := 0; i < len(data_nCov.Results[0].Cities); i++ {
-		fmt.Println(tm, data_nCov.Results[0].Cities[i].CityName, data_nCov.Results[0].Cities[i].CurrentConfirmedCount, data_nCov.Results[0].Cities[i].ConfirmedCount, data_nCov.Results[0].Cities[i].SuspectedCount, data_nCov.Results[0].Cities[i].CuredCount, data_nCov.Results[0].Cities[i].DeadCount)
+		row1 := []string{tm1, data_nCov.Results[0].Cities[i].CityName, strconv.Itoa(data_nCov.Results[0].Cities[i].CurrentConfirmedCount), strconv.Itoa(data_nCov.Results[0].Cities[i].ConfirmedCount), strconv.Itoa(data_nCov.Results[0].Cities[i].SuspectedCount), strconv.Itoa(data_nCov.Results[0].Cities[i].CuredCount), strconv.Itoa(data_nCov.Results[0].Cities[i].DeadCount)}
+		data2 = append(data2, row1)
 	}
+
+	// i := 1
 	// data1 := [][]string{
-	// 	{"12/14", "", "", "d", "d", "d", "d"},
+
+	// 	{tm1, data_nCov.Results[0].Cities[i].CityName, strconv.Itoa(data_nCov.Results[0].Cities[i].CurrentConfirmedCount), strconv.Itoa(data_nCov.Results[0].Cities[i].ConfirmedCount), strconv.Itoa(data_nCov.Results[0].Cities[i].SuspectedCount), strconv.Itoa(data_nCov.Results[0].Cities[i].CuredCount), strconv.Itoa(data_nCov.Results[0].Cities[i].DeadCount)},
 	// 	{"12/14", "", "", "d", "d", "d", "d"},
 	// 	{"12/14", "dd", "dd", "s", "h", "c", "s"},
 	// }
 
-	// tableList := tablewriter.NewWriter(os.Stdout)
-	// tableList.SetHeader([]string{"时间", "所在地", "现存确诊", "累计确诊", "可疑计数", "治愈数", "死亡人数"})
-	// tableList.SetFooter([]string{"", "", "sd", "", "sd", "", ""})
-	// tableList.SetBorder(false)
+	tableList := tablewriter.NewWriter(os.Stdout)
+	tableList.SetHeader([]string{"数据更新时间", "所在地", "现存确诊", "累计确诊", "可疑计数", "治愈数", "死亡人数"})
+	tableList.SetFooter([]string{tm1, data_nCov.Results[0].ProvinceName, strconv.Itoa(data_nCov.Results[0].CurrentConfirmedCount), strconv.Itoa(data_nCov.Results[0].ConfirmedCount), strconv.Itoa(data_nCov.Results[0].SuspectedCount), strconv.Itoa(data_nCov.Results[0].CuredCount), strconv.Itoa(data_nCov.Results[0].DeadCount)})
+	tableList.SetBorder(false)
 
-	// tableList.SetHeaderColor(
-	// 	tablewriter.Colors{tablewriter.Bold, tablewriter.BgGreenColor},
-	// 	tablewriter.Colors{tablewriter.FgHiRedColor, tablewriter.Bold, tablewriter.BgBlackColor},
-	// 	tablewriter.Colors{tablewriter.BgRedColor, tablewriter.FgWhiteColor},
-	// 	tablewriter.Colors{tablewriter.BgCyanColor, tablewriter.FgWhiteColor},
-	// 	tablewriter.Colors{tablewriter.BgCyanColor, tablewriter.FgWhiteColor},
-	// 	tablewriter.Colors{tablewriter.BgCyanColor, tablewriter.FgWhiteColor},
-	// 	tablewriter.Colors{tablewriter.BgCyanColor, tablewriter.FgWhiteColor})
+	tableList.SetHeaderColor(
+		tablewriter.Colors{tablewriter.Bold, tablewriter.BgGreenColor},
+		tablewriter.Colors{tablewriter.FgHiRedColor, tablewriter.Bold, tablewriter.BgBlackColor},
+		tablewriter.Colors{tablewriter.BgRedColor, tablewriter.FgWhiteColor},
+		tablewriter.Colors{tablewriter.BgCyanColor, tablewriter.FgWhiteColor},
+		tablewriter.Colors{tablewriter.BgCyanColor, tablewriter.FgWhiteColor},
+		tablewriter.Colors{tablewriter.BgCyanColor, tablewriter.FgWhiteColor},
+		tablewriter.Colors{tablewriter.BgCyanColor, tablewriter.FgWhiteColor})
 
-	// tableList.SetColumnColor(
-	// 	tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiBlackColor},
-	// 	tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiRedColor},
-	// 	tablewriter.Colors{tablewriter.Bold, tablewriter.FgBlackColor},
-	// 	tablewriter.Colors{tablewriter.Bold, tablewriter.FgBlackColor},
-	// 	tablewriter.Colors{tablewriter.Bold, tablewriter.FgBlackColor},
-	// 	tablewriter.Colors{tablewriter.Bold, tablewriter.FgBlackColor},
-	// 	tablewriter.Colors{tablewriter.Bold, tablewriter.FgBlackColor},
-	// )
+	tableList.SetColumnColor(
+		tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiBlackColor},
+		tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiRedColor},
+		tablewriter.Colors{tablewriter.Bold, tablewriter.FgBlackColor},
+		tablewriter.Colors{tablewriter.Bold, tablewriter.FgBlackColor},
+		tablewriter.Colors{tablewriter.Bold, tablewriter.FgBlackColor},
+		tablewriter.Colors{tablewriter.Bold, tablewriter.FgBlackColor},
+		tablewriter.Colors{tablewriter.Bold, tablewriter.FgBlackColor},
+	)
 
-	// tableList.SetFooterColor(
-	// 	tablewriter.Colors{}, tablewriter.Colors{},
-	// 	tablewriter.Colors{tablewriter.Bold}, tablewriter.Colors{},
-	// 	tablewriter.Colors{tablewriter.FgHiRedColor}, tablewriter.Colors{}, tablewriter.Colors{},
-	// )
+	tableList.SetFooterColor(
+		tablewriter.Colors{tablewriter.Bold}, tablewriter.Colors{tablewriter.FgHiRedColor},
+		tablewriter.Colors{tablewriter.FgHiRedColor}, tablewriter.Colors{tablewriter.FgHiRedColor},
+		tablewriter.Colors{tablewriter.FgHiRedColor}, tablewriter.Colors{tablewriter.FgHiRedColor}, tablewriter.Colors{tablewriter.Bold},
+	)
 
+	tableList.AppendBulk(data2)
 	// tableList.AppendBulk(data1)
-	// tableList.Render()
+	tableList.Render()
 
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/atomicgo/cursor v0.0.1 // indirect
 	github.com/gookit/color v1.5.0 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/olekukonko/tablewriter v0.0.5
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
 	golang.org/x/sys v0.0.0-20211214150614-024a26f5d6e2 // indirect
